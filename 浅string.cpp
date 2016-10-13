90f087480d86bb96edbd13f14e0803949c47af5e
cpp <1600977813@qq.com>
2016-10-13 18:43:48 +0800

    string类的浅拷贝

---
+++ 浅string.cpp
@@ -0,0 +1,48 @@
+
+
+class string
+{
+public:
+	
+		/*string()
+			:_pstr(new char[1])
+		{
+			*_pstr='\0';
+		}*/
+		string(const char* pstr = "")
+		{
+			if(NULL == pstr)
+			{
+				_pstr = new char[1];
+				*_pstr = '\0';
+			}
+			else
+			{
+				_pstr = new char[strlen(pstr)+1];
+				strcpy(_pstr,pstr);
+			}
+		}
+		string(const string& t)
+			:_pstr(t._pstr)
+		{}
+		string& operator =(const string& t)
+		{
+			if(this !=  &t)
+			{
+				_pstr = t._pstr;
+			}
+8			return *this;
+		}
+		~string()
+		{
+			if( NULL == _pstr)
+			{
+				delete[] _pstr;
+				_pstr = NULL;
+			}
+		}
+	
+
+private:
+	char* _pstr;
+}
